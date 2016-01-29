03d4a1edde6825e8d5beca5bcc04fecb4e3fdf86
MRohde89 <rheinsberg1@gmx.net>
2016-01-29 10:03:28 +0100

    udacitask starter tasks completed. No additional tasks yet

--- todolist.rb
+++ todolist.rb
@@ -1,6 +1,6 @@
 class TodoList
 
-    attr_reader :title, :items
+    attr_accessor :title, :items
 
     #attr_accessor :title
     def initialize(list_title)
@@ -8,19 +8,43 @@ class TodoList
     	@items = Array.new # starts empty! No items yet!
     end
 
+    def add_item(new_item)
+        item = Item.new(new_item)
+        @items.push(item)
+    end
+
+    def delete_item(index)
+      @items.delete_at(index)
+    end
+
+    def item_completed(index)
+      @items[index].completed_status = "true"
+    end
+
+    def show
+
+      puts self.title.center(20, '*')
+      puts
+      self.items.each do |item|
+       puts "#{item.description.ljust(60, ' -')} Status: #{item.completed_status}"
+      end
+      puts
+    end
+
+    def completed?(item)
+      (@items.select {|item| item.description == item}).completed_status
+    end
+
+
+
 end
 
 class Item
-  attr_reader :description, :completed_status
+  attr_accessor :description, :completed_status
 
   def initialize(item_description)
     @description =  item_description
     @completed_status = false
-  end
-
-  def add_item(new_item)
-      item = Item.new(new_item)
-      @items.push(item)
   end
 
 end
