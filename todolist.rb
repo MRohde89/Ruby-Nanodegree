b894dd676e240ca9dedcd09141fdac14252782a4
MRohde89 <rheinsberg1@gmx.net>
2016-01-29 20:21:15 +0100

    modified to have multiple Lists which is bound in a hash. There is still a lot to do, but many methods are already working fine. Modified now working methods: add_list, current_list, change_to_list, show_lists, change_name_to. The rest needs to be modified to work for multiple lists as well.

--- todolist.rb
+++ todolist.rb
@@ -1,12 +1,50 @@
 class TodoList
 
-    attr_accessor :title, :items
+    attr_accessor :title, :items, :current_list, :hash_tag, :current_index
 
     #attr_accessor :title
     def initialize(list_title)
-    	@title = list_title
+    	@hash_tag = Hash.new # starts empty Hash
     	@items = Array.new # starts empty! No items yet!
+      @hash_tag[list_title] = []
+      @current_list = list_title
+      @current_index = 0
     end
+
+    def add_list(list_title)
+      @hash_tag[list_title] = []
+      return "List with Title #{list_title} has been created"
+    end
+
+    def current_list
+      return @current_list
+    end
+
+    def change_to_list(list)
+      if
+      self.hash_tag.keys.include? list
+      @current_list = list
+      return "List has been changed to #{current_list}"
+    else
+      return "No such list"
+    end
+    end
+
+    def show_lists
+      return self.hash_tag.keys
+    end
+
+    def change_name_to(new_title)
+      replacement = { @current_list => new_title }
+      # Renaming Solution for hash keys from Stackoverflow: http://stackoverflow.com/questions/4137824/how-to-elegantly-rename-all-keys-in-a-hash-in-ruby
+      self.hash_tag.keys.each { |k| self.hash_tag[replacement[k]] = self.hash_tag.delete(k) if replacement[k]}
+      @current_list = new_title
+      @current_index = list.hash_tag.key.length -1 # because the changed part will be at the end of the array
+      return "Listname changed to #{new_title}"
+    end
+####################
+##### NOT ADJUSTED TO MULTIPLE LISTS
+####################
 
     def add_item(new_item)
         item = Item.new(new_item)
@@ -20,16 +58,6 @@ class TodoList
     def item_completed(index)
       @items[index].completed_status = "true"
     end
-
-    def show
-      puts self.title.center(20, '*')
-      puts
-      self.items.each do |item|
-       puts "#{item.description.ljust(60, ' -')} Status: #{item.completed_status}"
-      end
-      puts
-    end
-
 
     def show(output_method = 0)
 
@@ -53,23 +81,10 @@ class TodoList
       end
     end
 
-
-
     def completed?(index)
       @items[index].completed_status
     end
 
-    def to_file(directory_and_file = "report.txt")
-      output = File.new(directory_and_file, 'w+')
-      output.puts self.title.center(20, '*')
-      output.puts
-      self.items.each do |item|
-        output.puts "#{item.description.ljust(60, ' -')} Status: #{item.completed_status}"
-      end
-      output.puts
-      output.close
-      return "Output to file #{directory_and_file}, since no file name was specified"
-    end
 
     def to_file(directory_and_file = "report.txt")
       output = File.new("report.txt", 'w+')
@@ -78,6 +93,9 @@ class TodoList
       return "Output to file #{directory_and_file}"
     end
 
+    def another_list(title)
+      #@todolists.push(self)
+    end
 
 end
 
